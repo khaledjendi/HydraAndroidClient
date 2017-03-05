28b71409ddf10d21e65c246d63e1ebdfe0d3bca4
khaled <khaledj@kth.se>
2017-03-05 01:41:07 +0100

    Design game activity layout

---
+++ app/src/main/java/com/client/activity/GameActivity.java
@@ -0,0 +1,15 @@
+package com.client.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.client.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
